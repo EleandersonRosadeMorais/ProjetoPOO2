7bc3f84c89cfd3137b5f35b79b03c58012f71d2a
MariTrindadee <mari.florestb@gmail.com>
2025-09-01 22:03:17 -0300

    Primeiro commit

---
+++ AulaLegal/src/aulalegal/AulaLegal.java
@@ -0,0 +1,15 @@
+
+package aulalegal;
+
+import view.FrLogin;
+
+public class AulaLegal {
+
+    public static void main(String[] args) {
+        
+        FrLogin telaLogin = new FrLogin();
+        telaLogin.setVisible(true);
+        
+    }
+    
+}
